b4acba5c56b475e7ca8d8c48b2d74199b0ac37b9
Jessie Linsangan <jessielinsangan@gmail.com>
2023-01-16 10:09:04 -0600

    Spinner Comments Commit

--- stylesheets/loader.js
+++ stylesheets/loader.js
@@ -1,12 +1,19 @@
 /*
-  Code from https://codepen.io/dcode-software/pen/rNYGdeg 
+  Code from https://www.youtube.com/watch?v=-HS9IIuT_Mo
+
+  Once the page has fully loaded,
+
+  document.querySelector(.loader) selects the elements with a class of loader
+  loader.classList.add("loader--hidden"); adds the class of loader--hidden, which makes the spinner fade away
+  
+  Once the transition has finished,
+
+  document.body.removeChild(document.querySelector(".loader")); removes the loader class from the element
 */
 window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
-
-  loader.classList.add("loader--hidden");
+  document.querySelector(".loader").classList.add("loader--hidden");
 
   loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
+    document.body.removeChild(document.querySelector(".loader"));
   });
 });
